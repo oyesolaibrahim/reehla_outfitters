39634ddf6774557fae5372648b68b953478e8b95
Ibrahim <oyesolaibrahim14@gmail.com>
2024-05-30 09:10:05 +0100

    outdated packages updated

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -1,5 +1,5 @@
 const Admin = require("../models/admin.model"); 
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const jwtDecode = require("jwt-decode");
 
--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const User = require("../models/user.model"); 
 const Clients = require("../models/userCheckout.model"); 
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const jwtDecode = require("jwt-decode");
 const axios = require('axios');
