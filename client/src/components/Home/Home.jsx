e2f5f1f97229f6ee994bfd1a74e0cc02639edfad
Ibrahim <oyesolaibrahim14@gmail.com>
2024-05-31 11:12:35 +0100

    Adjustment on mobile view

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,30 +4,41 @@ import FirstSection from "./Sections/FirstSEction";
 import SecondSection from "./Sections/SecondSection";
 import ThirdSection from "./Sections/ThirdSection";
 import FourthSection from "./Sections/FourthSection";
+import { useEffect } from "react";
 
 const Home = () => {
-    
-    const userToken = sessionStorage.getItem("userToken");
-    const FirstName = sessionStorage.getItem("User");
 
-    const textStyle = {
-        fontFamily: "'Dancing Script', cursive"
-    };
+    useEffect(() => {
+        const textElements = document.querySelectorAll('.typing-text');
+        
+        textElements.forEach((textElement) => {
+            const text = textElement?.innerText;
+            let index = 0;
+            textElement.innerText = '';
+
+            function typeWriter() {
+                if (index < text.length) {
+                    textElement.innerHTML += text.charAt(index);
+                    index++;
+                    setTimeout(typeWriter, 50);
+                }
+            }
+
+            typeWriter();
+        });
+    }, []);  
     
     return (
         <>  
         <Header/>
             <main className="bg-gray-700 pb-10 overflow-x-hidden">
                 <div className="background">
-                    <div className="text-container text-white text-center justify-center bg-red-800 py-5 px-10 lg:w-4/5 md:w-screen mx-auto">
-                        <h1 className="lg:text-4xl md:text-3xl xs:text-2xl sm:text-2xl font-extrabold mb-4 typing-container">
-                            <span style={{fontFamily: "Dancing Script"}} className="typing-effect text-center">Welcome to Real Babs Jalab and Fragrance Store!</span>
-                        </h1>
-                        <p className="md:text-xl typing-container">
-                            <span  className="typing-effect" style={{animationDelay: '4s', fontFamily: "Dancing Script" }}>Discover the finest selection of Jalabs and luxurious fragrances,</span>
-                            <span  className="typing-effect" style={{fontFamily: "Dancing Script", animationDelay: '7s' }}> crafted to elevate your style and essence.</span>
-                            <span  className="typing-effect" style={{ animationDelay: '10s', fontFamily: "Dancing Script" }}> Let our exclusive collections inspire and transform</span>
-                            <span  className="typing-effect" style={{ animationDelay: '13s', fontFamily: "Dancing Script" }}> your everyday moments into extraordinary experiences.</span>
+                    <div className="text-container text-white text-center justify-center py-5 px-10 lg:w-4/5 md:w-screen mx-auto">
+                        <p className="typing-text text-4xl font-extrabold mb-4" style={{ fontFamily: "'Dancing Script', cursive" }}>
+                            Welcome to Real Babs Jalab and Fragrance Store!
+                        </p>
+                        <p className="typing-text text-xl" style={{ fontFamily: "'Dancing Script', cursive" }}>
+                            Discover the finest selection of Jalabs and luxurious fragrances, crafted to elevate your style and essence. Let our exclusive collections inspire and transform your everyday moments into extraordinary experiences.
                         </p>
                     </div>
                 </div>
