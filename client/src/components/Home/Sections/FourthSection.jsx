3a8ddf9cbd9f6537672b5b9c2d6ab2f65a931778
Ibrahim <oyesolaibrahim14@gmail.com>
2024-06-10 17:32:47 +0100

    Controllers Adjustment

--- client/src/components/Home/Sections/FourthSection.jsx
+++ client/src/components/Home/Sections/FourthSection.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 const FourthSection = () => {
 const [visible, setVisible] = useState(false);
@@ -19,7 +20,9 @@ const element = (e) => {
         <>  
             <section className={`flex justify-end sm:right-12 md:right-12 xs:right-5 fixed sm:bottom-32 md:bottom-32 xs:bottom-28 items-center space-x-4 ${visible ? "opacity-100 animate__animated animate__bounce" : "opacity-0"}`}>
                 {/* <h3 className="font-bold rounded-lg bg-amber-800 py-3 px-5 xs:text-xs text-white toolpoint">Message us now</h3> */}
-                <i className="fa fa-whatsapp sm:text-2xl md:text-7xl xs:text-5xl text-green-500 cursor-pointer"  aria-hidden="true"></i>
+                <Link to="https://wa.link/hwaf21">
+                    <i className="fa fa-whatsapp sm:text-2xl md:text-7xl xs:text-5xl text-green-500 cursor-pointer"  aria-hidden="true"></i>
+                </Link>
             </section>
         </>
     )
