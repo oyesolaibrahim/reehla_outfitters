4b11b241180634808b1b71947f126b7ee8361249
Ibrahim <oyesolaibrahim14@gmail.com>
2024-06-10 17:42:59 +0100

    Controllers Adjustment

--- client/src/components/Home/Sections/FourthSection.jsx
+++ client/src/components/Home/Sections/FourthSection.jsx
@@ -18,12 +18,12 @@ const element = (e) => {
 }
     return (
         <>  
+            <Link to="https://wa.link/hwaf21">
             <section className={`flex justify-end sm:right-12 md:right-12 xs:right-5 fixed sm:bottom-32 md:bottom-32 xs:bottom-28 items-center space-x-4 ${visible ? "opacity-100 animate__animated animate__bounce" : "opacity-0"}`}>
                 {/* <h3 className="font-bold rounded-lg bg-amber-800 py-3 px-5 xs:text-xs text-white toolpoint">Message us now</h3> */}
-                <Link to="https://wa.link/hwaf21">
-                    <i className="fa fa-whatsapp sm:text-2xl md:text-7xl xs:text-5xl text-green-500 cursor-pointer"  aria-hidden="true"></i>
-                </Link>
+                    <i className="fa fa-whatsapp sm:text-5xl md:text-7xl xs:text-5xl text-green-500 cursor-pointer"  aria-hidden="true"></i>
             </section>
+            </Link>
         </>
     )
 }
